da9a5304b6d47d7935b356786c095617bc4c48f8
HyoSoo HAN <nemosoo5621@gmail.com>
2025-04-24 12:00:44 +0900

    fix : alter_table 쿼리 수정
    Refs : https://illunex.daouoffice.com/app/works/applet/9139/doc/518525/navigate

--- src/main/resources/db/common/alter_table.sql
+++ src/main/resources/db/common/alter_table.sql
@@ -1,2 +1,4 @@
-ALTER TABLE em_saas.project ADD partnership_idx INT(11) NULL DEFAULT NULL COMMENT '프로젝트 카테고리번호' AFTER idx;
-ALTER TABLE em_saas.project ADD delete_date DATETIME NULL DEFAULT NULL COMMENT '프로젝트 삭제일' AFTER create_date;
+ALTER TABLE `em_saas`.`project`
+    ADD COLUMN IF NOT EXISTS `partnership_idx` INT(11) NULL DEFAULT NULL COMMENT '프로젝트 카테고리번호' AFTER `idx`;
+ALTER TABLE `em_saas`.`project`
+    ADD COLUMN IF NOT EXISTS `delete_date` DATETIME NULL DEFAULT NULL COMMENT '프로젝트 삭제일' AFTER `create_date`;
