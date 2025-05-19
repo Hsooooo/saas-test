7ef89e2e28e931447eb101722b6f788855b395d4
hs-leeee <hs.lee@illunex.com>
2025-05-19 14:13:49 +0900

    modify : 회원 로그인 이력 컬럼 길이 변경

--- src/main/resources/db/common/alter_table.sql
+++ src/main/resources/db/common/alter_table.sql
@@ -2,3 +2,7 @@ ALTER TABLE `em_saas`.`project`
     ADD COLUMN IF NOT EXISTS `partnership_idx` INT(11) NULL DEFAULT NULL COMMENT '프로젝트 카테고리번호' AFTER `idx`;
 ALTER TABLE `em_saas`.`project`
     ADD COLUMN IF NOT EXISTS `delete_date` DATETIME NULL DEFAULT NULL COMMENT '프로젝트 삭제일' AFTER `create_date`;
+ALTER TABLE `em_saas`.`member_login_history`
+    MODIFY COLUMN `browser` VARCHAR(255) NULL DEFAULT NULL COMMENT '접속 브라우저',
+    MODIFY COLUMN `platform` VARCHAR(255) NULL DEFAULT NULL COMMENT '접속 환경';
+
