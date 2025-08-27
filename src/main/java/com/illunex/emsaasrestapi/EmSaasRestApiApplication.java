a2bd03910336084c0f24c66b0673f41879a6f169
hs-leeee <hs.lee@illunex.com>
2025-08-27 11:36:12 +0900

    modify : 엑셀 파싱 로직 배열 상한 관련 제한 수정

--- src/main/java/com/illunex/emsaasrestapi/EmSaasRestApiApplication.java
+++ src/main/java/com/illunex/emsaasrestapi/EmSaasRestApiApplication.java
@@ -15,7 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableMethodSecurity(prePostEnabled = true)
 public class EmSaasRestApiApplication {
     public static void main(String[] args) {
-        IOUtils.setByteArrayMaxOverride(100 * 1024 * 1024); // 100MB
+        IOUtils.setByteArrayMaxOverride(200 * 1024 * 1024); // 100MB
         SpringApplication.run(EmSaasRestApiApplication.class, args);
     }
 
