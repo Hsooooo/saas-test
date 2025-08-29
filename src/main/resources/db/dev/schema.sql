ffd22047c8fb84a3428dac7e56d896d45b8c1e78
Hsooooo <rkrk6469@nate.com>
2025-08-29 09:56:51 +0900

    modify : 외부 도구 결과 테이블 스키마 추가

--- src/main/resources/db/dev/schema.sql
+++ src/main/resources/db/dev/schema.sql
@@ -409,3 +409,20 @@ CREATE TABLE IF NOT EXISTS `em_saas`.`project_query` (
 COMMENT='프로젝트 쿼리 정보'
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB;
+
+CREATE TABLE `em_saas`.`chat_tool_result` (
+    `idx` INT(11) NOT NULL AUTO_INCREMENT COMMENT '채팅 외부 도구 결과 번호',
+    `chat_history_idx` INT(11) NULL DEFAULT NULL COMMENT '채팅 이력 번호',
+    `tool_type` VARCHAR(7) NULL DEFAULT NULL COMMENT '채팅 도구 타입' COLLATE 'utf8mb4_general_ci',
+    `title` VARCHAR(255) NULL DEFAULT NULL COMMENT '검색 결과 제목' COLLATE 'utf8mb4_general_ci',
+    `url` VARCHAR(255) NULL DEFAULT NULL COMMENT '검색 결과 url' COLLATE 'utf8mb4_general_ci',
+    `update_date` DATETIME NULL DEFAULT NULL COMMENT '수정일',
+    `create_date` DATETIME NULL DEFAULT NULL COMMENT '생성일',
+    PRIMARY KEY (`idx`) USING BTREE,
+    INDEX `fk_chat_tool_result_chat_history_idx` (`chat_history_idx`) USING BTREE,
+    CONSTRAINT `fk_chat_tool_result_chat_history_idx` FOREIGN KEY (`chat_history_idx`) REFERENCES `chat_history` (`idx`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COMMENT='LLM 채팅 외부 도구 결과'
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB;
+;
