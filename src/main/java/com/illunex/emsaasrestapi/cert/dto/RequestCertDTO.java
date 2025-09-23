961d1c19e1af3659887d2c1d6a694a51db71a7db
hs-leeee <hs.lee@illunex.com>
2025-09-23 15:04:45 +0900

    fix : 인증 메일 오류 수정

--- src/main/java/com/illunex/emsaasrestapi/cert/dto/RequestCertDTO.java
+++ src/main/java/com/illunex/emsaasrestapi/cert/dto/RequestCertDTO.java
@@ -2,20 +2,21 @@ package com.illunex.emsaasrestapi.cert.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class RequestCertDTO {
 
-    @Builder
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Cert {
         private String certData;
     }
 
-    @Builder
     @Setter
     @Getter
+    @NoArgsConstructor
     public static class InviteSignup {
         private String certData;
         private String password;
