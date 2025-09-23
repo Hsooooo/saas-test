89b3e4600ea748fa857582a4f82d320048bb8ca7
hs-leeee <hs.lee@illunex.com>
2025-09-23 14:10:45 +0900

    fix : 오ㅓ류 수정

--- src/main/java/com/illunex/emsaasrestapi/cert/dto/RequestCertDTO.java
+++ src/main/java/com/illunex/emsaasrestapi/cert/dto/RequestCertDTO.java
@@ -1,18 +1,21 @@
 package com.illunex.emsaasrestapi.cert.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 public class RequestCertDTO {
 
+    @Builder
     @Getter
     @Setter
     public static class Cert {
         private String certData;
     }
 
-    @Getter
+    @Builder
     @Setter
+    @Getter
     public static class InviteSignup {
         private String certData;
         private String password;
